04ffb4e249211aee5d19c849afaff546366804b1
OkemotoHiroki <okemoto@antas.jp>
2026-04-15 14:58:55 +0900

    Update database.yml and CI workflow

---
+++ test/system/samples_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class SamplesTest < ApplicationSystemTestCase
+  # test "visiting root" do
+  #   visit root_url
+
+  #   assert_selector "body"
+  # end
+end
